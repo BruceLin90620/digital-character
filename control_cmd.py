4242f60f12ef8b48ceb5a706f429252fcf81846f
bruce <BruceLin90620>
2023-11-28 17:05:23 +0800

    commit 1128

---
+++ control_cmd.py
@@ -0,0 +1,61 @@
+from DXL_motor_control import DXL_Conmunication
+
+DEVICE_NAME = "/dev/ttyUSB0"
+B_RATE      = 57600
+   
+class ControlCmd:
+    def __init__(self):
+        self.dynamixel = DXL_Conmunication(DEVICE_NAME, B_RATE)
+        self.dynamixel.activateDXLConnection()
+        motor0 = self.dynamixel.createMotor('motor0', motor_number=0)
+        motor1 = self.dynamixel.createMotor('motor1', motor_number=1)
+        motor2 = self.dynamixel.createMotor('motor2', motor_number=2)
+        motor3 = self.dynamixel.createMotor('motor3', motor_number=3)
+        motor4 = self.dynamixel.createMotor('motor4', motor_number=4)
+        motor5 = self.dynamixel.createMotor('motor5', motor_number=5)
+        motor6 = self.dynamixel.createMotor('motor6', motor_number=6)
+        motor7 = self.dynamixel.createMotor('motor7', motor_number=7)
+        motor8 = self.dynamixel.createMotor('motor8', motor_number=8)
+        motor9 = self.dynamixel.createMotor('motor9', motor_number=9)
+
+        self.motor_list = [motor0, motor1, motor2, motor3, motor4, 
+                    motor5, motor6, motor7, motor8, motor9]
+        
+        self.motor_position = { "motor0":0, "motor1":0, "motor2":0, "motor3":0, "motor4":0,
+                                "motor5":0, "motor6":0, "motor7":0, "motor8":0, "motor9":0}
+        self.dynamixel.updateMotorData()
+
+    def disable_all_motor(self):
+        for motor in self.motor_list:
+            motor.disableMotor()
+        self.dynamixel.closeHandler()
+
+    def read_motor_data(self):
+        for motor in self.motor_list:
+            position, ret = motor.directReadData(132, 4)
+            self.motor_position[motor.name] = position
+            # print("motor id:", motor.DXL_ID, "motor position:", position)
+        return self.motor_position
+
+
+if __name__ == "__main__":
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+    
+    controlcmd = ControlCmd()
+
+    while True:
+        print("Press any key to continue! (or press ESC to quit!)")
+        if getch() == chr(0x1b):
+            break
+        all_servo_position = controlcmd.read_motor_data()
+        print(all_servo_position)
+    controlcmd.disable_all_motor()
---
+++ robot_socket_server.py
@@ -0,0 +1,56 @@
+import threading
+import socket
+import json
+from control_cmd import ControlCmd
+
+class Server():
+    def __init__(self, server_ip, unity_ip, port):
+
+        self.incoming_TCP_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.incoming_TCP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.incoming_TCP_socket.bind((server_ip, port))
+        self.incoming_TCP_socket.listen(5)
+
+        self.unity_ip = unity_ip
+
+        self.unitySocketClient = UnitySocketClient()
+
+    def scanForClientConnection(self):
+        while 1:
+            tcp_client_conn, tcp_client_addr = self.incoming_TCP_socket.accept()
+            print(tcp_client_addr[0], self.unity_ip)
+            if tcp_client_addr[0] == self.unity_ip:
+                self.unitySocketClient.scanForUnityClientConnection(tcp_client_conn, tcp_client_addr)
+
+class UnitySocketClient():
+    def __init__(self):
+        self.control_cmd = ControlCmd()
+    
+    def handleClient(self, client_socket, addr):
+        while True:
+            try:
+                all_servo_position = self.control_cmd.read_motor_data()
+                msg = json.dumps(all_servo_position)
+                
+                client_socket.send(bytes(msg, encoding="utf-8"))
+            except InterruptedError:
+                print("InterruptedError")
+                client_socket.close()
+
+    def scanForUnityClientConnection(self, unity_client_conn, unity_client_addr):
+        try:
+            print("Connect to Unity Client Success, addr: {}".format(unity_client_addr))
+            client_thread = threading.Thread(target=self.handleClient, args=(unity_client_conn, 
+                                            unity_client_addr))
+            client_thread.start()
+    
+        except socket.error:
+            print(socket.error)
+
+if __name__ == "__main__":
+    server_ip = '10.100.3.18'
+    unity_ip = '10.100.3.18' 
+    port = 8000
+
+    demo_server = Server(server_ip, unity_ip, port)
+    demo_server.scanForClientConnection()
